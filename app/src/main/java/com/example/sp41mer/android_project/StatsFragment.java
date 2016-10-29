2987db1c4841c4c505cbfc92d99a78cf80c4595e
sp41mer <sp41mer@yahoo.com>
2016-10-29 22:21:51 +0300

    new Fragment + button on card

--- app/src/main/java/com/example/sp41mer/android_project/StatsFragment.java
+++ app/src/main/java/com/example/sp41mer/android_project/StatsFragment.java
@@ -3,6 +3,7 @@ package com.example.sp41mer.android_project;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,8 +25,23 @@ public class StatsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_stats, container, false);
+        Button button= (Button) rootView.findViewById(R.id.more_info);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                OnePhotoFragment fragment = new OnePhotoFragment();
+                //Не уверен что так можно, но индус сказал делать так
+                android.support.v4.app.FragmentTransaction fragmentTransaction =
+                        getFragmentManager().beginTransaction();
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.commit();
+            }
+
+        });
+
         // Inflate the layout for this fragment
         return rootView;
+
+
     }
 
 }
