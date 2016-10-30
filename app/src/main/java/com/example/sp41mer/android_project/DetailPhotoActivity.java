3411184474cff1fb7200078aa68fa199a7f4cf52
maratkanov-a <a.s.maratkanov@gmail.com>
2016-10-30 22:47:44 +0300

    connected to second activity

--- app/src/main/java/com/example/sp41mer/android_project/DetailPhotoActivity.java
+++ app/src/main/java/com/example/sp41mer/android_project/DetailPhotoActivity.java
@@ -7,7 +7,7 @@ public class DetailPhotoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.fragment_one_photo);
     }
 
     @Override
