8db9ebe0602d09fe702a020fb7f74e7ba96b11db
sp41mer <sp41mer@yahoo.com>
2016-12-15 14:01:15 +0300

    ebanii integtation hell.vot teper  otkatil

--- app/src/main/java/com/technopark/dreamteam/moneybox/MainActivity.java
+++ app/src/main/java/com/technopark/dreamteam/moneybox/MainActivity.java
@@ -286,4 +286,10 @@ public class MainActivity extends AppCompatActivity
             startService(intent);
         }
     }
+
+    @Override
+    protected void onStop() {
+        super.onDestroy();
+        statusReciever.registerCallback(null);
+    }
 }
